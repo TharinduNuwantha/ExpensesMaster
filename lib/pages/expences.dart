38e1fd2b1af41d63864932edddf86c39de07c073
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2026-03-04 00:15:24 +0530

    first commit

---
+++ lib/pages/expences.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Expences extends StatelessWidget {
+  const Expences({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
